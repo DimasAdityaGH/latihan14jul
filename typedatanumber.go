9243c68cf66f033143f7cfebaa6c067e1ebdd037
dimasAditya <dimasubuntu21@gmail.com>
2021-07-14 19:53:58 +0700

    create type data number

---
+++ typedatanumber.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nilai8 int8 = 127
+	var nilai16 int16 = 32767
+	var nilai32 int32 = 2323233
+	var nilai64 int64 = 12121334344344
+	fmt.Println(nilai8)
+	fmt.Println(nilai16)
+	fmt.Println(nilai32)
+	fmt.Println(nilai64)
+}
