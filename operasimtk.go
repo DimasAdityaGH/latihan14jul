5c7e0cd9bf99f6ea349bb21b5d30bf5ebb03ebd1
dimasAditya <dimasubuntu21@gmail.com>
2021-07-14 19:41:34 +0700

    create operasi matematika

---
+++ operasimtk.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main () {
+	var a = 10
+	var b = 10
+	var c = a * b
+	fmt.Println(c)
+
+	var i = 10
+	i++ 
+	i += 9
+	fmt.Println(i)
+
+	var k = 10
+	k *= 10
+	fmt.Println(k)
+
+	var hasil = 10 * 20
+	fmt.Println(hasil)
+	
+}
